e07213edac5b030ff2f7fa47e6ab859433fc2547
Atjie <122915226+Atjie@users.noreply.github.com>
2023-04-05 18:58:38 -0400

    styling

--- app.py
+++ app.py
@@ -1,25 +1,41 @@
 from flask import Flask, render_template, request
-from mbta_helper import find_stop_near
-
+from mbta_helper import find_stop_near, get_nearest_station_coor
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def index():
+def get_map():
     return render_template("index.html")
 
-@app.get("/station") #METHOD NOT ALLOWED #Attribute error
-def get_station():
-    return render_template("station-form.html")
 
-@app.post('/station')
-def stop_post():
-    place_name = request.form.get('location')
-    place_name = str(place_name)
-    stop, wheelchair = find_stop_near(place_name)
-    return render_template('station-result.html', stop=stop, wheelchair=wheelchair)
-    
+ACTIONS = ["Show Map"]
+
+
+@app.get("/station/")
+def get_register():
+    return render_template("station-form.html", courses=ACTIONS)
+
+
+@app.post("/station/")
+def post_register():
+    action = request.form.get("action")
+    # if course not in ACTIONS:
+    #     return render_template("failure.html")
+    if action == "Show Map":
+        return show_map()
+    place_name = str(request.form.get("location"))
+    STOP, WHEELCHAIR = find_stop_near(place_name)
+    return render_template("station-result.html", stop=STOP, wheelchair=WHEELCHAIR)
+
+
+@app.route("/map")
+def show_map():
+    place_name = str(request.form.get("location"))
+    LATITUDE, LONGITUDE = get_nearest_station_coor(place_name)
+    html_map = f'<iframe src="https://maps.google.com/maps?q={LATITUDE},{LONGITUDE}&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=&amp;output=embed"></iframe>'
+    return html_map
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=3640)
--- mbta_helper.py
+++ mbta_helper.py
@@ -23,13 +23,12 @@ def get_lat_long(place_name: str) -> tuple[str, str]:
 
     See https://docs.mapbox.com/api/search/geocoding/ for Mapbox Geocoding API URL formatting requirements.
     """
-    place_name = place_name.replace(" ","+")    
+    place_name = place_name.replace(" ","+") + ',Boston,MA'    
     MAPBOX_BASE_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places"
     url = f'{MAPBOX_BASE_URL}/{place_name}.json?access_token={MAPBOX_TOKEN}'
     response_data = get_json(url)
-    # pprint(response_data)
-    [longitude, latitude] = response_data['features'][0]['center'] #coordinates are longitude latitude
-    return tuple([latitude, longitude])
+    longitude, latitude = response_data['features'][0]['center'] #coordinates are longitude then latitude
+    return latitude, longitude
 
 
 def get_nearest_station(latitude: str, longitude: str, sortby='distance', radius='0.01') -> tuple[str, bool]:
@@ -41,7 +40,7 @@ def get_nearest_station(latitude: str, longitude: str, sortby='distance', radius
     MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
     MBTA_url = f'{MBTA_BASE_URL}?api_key={MBTA_API_KEY}&sort={sortby}&filter%5Blatitude%5D={latitude}&filter%5Blongitude%5D={longitude}&radius%5D={radius}'
     # pprint(get_json(MBTA_url)) #: list
-    data = get_json(MBTA_url)['data'][0] #dict
+    data = get_json(MBTA_url)['data'][0] #: dict
     # pprint(data)
     station = data['attributes']['name']
     # print(station)
@@ -52,6 +51,22 @@ def get_nearest_station(latitude: str, longitude: str, sortby='distance', radius
     #     wheelchair_access = "No"
     wheelchair_access = "Yes" if wheelchair_access == 1 else "No"
     return station, wheelchair_access
+
+def get_nearest_station_coor(place_name: str, sortby='distance', radius='0.01') -> tuple[str, str]:
+    """
+    Given latitude and longitude strings, return a (latitude, longitude) tuple for the nearest MBTA station to the given current coordinates.
+
+    See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL formatting requirements for the 'GET /stops' API.
+    """
+    latitude, longitude = get_lat_long(place_name)
+    MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
+    MBTA_url = f'{MBTA_BASE_URL}?api_key={MBTA_API_KEY}&sort=distance&filter%5Blatitude%5D={latitude}&filter%5Blongitude%5D={longitude}&radius%5D={radius}'
+    # pprint(get_json(MBTA_url)) #: list
+    data = get_json(MBTA_url)['data'][0] #: dict
+    pprint(data)
+    station_latitude = data['attributes']['latitude']
+    station_longitude = data['attributes']['longitude']
+    return station_latitude, station_longitude
 
 
 def find_stop_near(place_name: str) -> tuple[str, bool]:
@@ -69,38 +84,37 @@ def main():
     """
     Testing of all the functions here.
     """ 
-    MAPBOX_BASE_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places"
-    query = "Babson+College"
-    url = f'{MAPBOX_BASE_URL}/{query}.json?access_token={MAPBOX_TOKEN}'
-    # url='https://api.mapbox.com/geocoding/v5/mapbox.places/Boston.json?access_token=pk.eyJ1IjoiYWJieXRqaWUiLCJhIjoiY2xnMDB1cGg3MTJwNTNlbWsyM2N5Zjc1ZyJ9.MuR9ei396i_ZGyZ43G6GvQ'
+    # MAPBOX_BASE_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places"
+    # query = "Babson+College"
+    # url = f'{MAPBOX_BASE_URL}/{query}.json?access_token={MAPBOX_TOKEN}'
+    # # url='https://api.mapbox.com/geocoding/v5/mapbox.places/Boston.json?access_token=pk.eyJ1IjoiYWJieXRqaWUiLCJhIjoiY2xnMDB1cGg3MTJwNTNlbWsyM2N5Zjc1ZyJ9.MuR9ei396i_ZGyZ43G6GvQ'
 
-    """
-    Hardcode: Kept getting "bad request" error, but then requested a new mapbox token without limit to url, attempted hard code
-    "Babson%College" 400 Error: Bad Request - the request could not be satisfied
-    When running, always gives "SyntaxError: invalid syntax" - but if you run and debug says "no bugs" and gives the output / pylance reload required
-    """
+    # """
+    # Hardcode: Kept getting "bad request" error, but then requested a new mapbox token without limit to url, attempted hard code
+    # "Babson%College" 400 Error: Bad Request - the request could not be satisfied
+    # When running, always gives "SyntaxError: invalid syntax" - but if you run and debug says "no bugs" and gives the output / pylance reload required
+    # """
 
-    """
-    Get Latitude and Longitude Tuples
-    """
-    Home = get_lat_long('Congress Street Boston')
-    print(Home)
-    # print(type(Home)) #tuple
-    # coordinates: (42.35188, -71.04957)
+    # """
+    # Get Latitude and Longitude Tuples
+    # """
+    # Home = get_lat_long('Congress Street Boston')
+    # print(Home)
+    # # print(type(Home)) #tuple
+    # # coordinates: (42.35188, -71.04957)
     
-    Gables_lat = 42.34845515783725
-    Gables_long = -71.04221613687264
-    print(get_nearest_station(Gables_lat, Gables_long))
+    # Gables_lat = 42.34845515783725
+    # Gables_long = -71.04221613687264
+    # print(get_nearest_station(Gables_lat, Gables_long))
 
-    NS_latitude, NS_longitude = get_lat_long('Newbury Street')
-    station = get_nearest_station(NS_latitude, NS_longitude)
-    print(station)
+    # NS_latitude, NS_longitude = get_lat_long('Newbury Street')
+    # station = get_nearest_station(NS_latitude, NS_longitude)
+    # print(station)
 
-    print(find_stop_near('Newbury Street'))
+    # print(find_stop_near('Newbury Street'))
+    # get_nearest_station_coor('newbury street')
+    print(find_stop_near('newbury street'))
 
 
-
-
-    
 if __name__ == '__main__':
     main()
